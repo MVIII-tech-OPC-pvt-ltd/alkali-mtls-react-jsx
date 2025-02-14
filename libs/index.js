30665e41f4a9ebaa80a9eca3def8074f9662d14b
Avi <avinashkumar@mviiitech.com>
2025-02-15 01:47:41 +0530

    export WareHouseProvider and UsersHubProvider from index.js

--- libs/index.js
+++ libs/index.js
@@ -2,5 +2,14 @@ import AlMNavigationTemplateApp from "./pages/AlMNavigationTemplateApp";
 import AlMTemplateApp from "./pages/AlMTemplateApp";
 import { WareHouseContext } from "./contexts/WareHouse";
 import { UsersHubContext } from "./contexts/UsersHub";
+import { WareHouseProvider } from "./contexts/WareHouse";
+import { UsersHubProvider } from "./contexts/UsersHub";
 
-export { AlMNavigationTemplateApp, AlMTemplateApp, WareHouseContext, UsersHubContext };
+export { 
+    AlMNavigationTemplateApp, 
+    AlMTemplateApp, 
+    WareHouseContext, 
+    UsersHubContext,
+    WareHouseProvider,
+    UsersHubProvider
+};
